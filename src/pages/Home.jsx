b59857ae8c07df0f4ae9463854817ef9c558a485
AuroreDevWeb <aurore.croizet@hotmail.fr>
2026-01-12 19:26:30 +0100

    ajout des pages + routes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="Home">
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is the home page of my personal portfolio website.</p>
+    </div>
+  );
+}
+export default Home;
---
+++ src/pages/Services.jsx
@@ -0,0 +1,9 @@
+function Services() {
+  return (
+    <div className="Services">
+      <h2>Our Services</h2>
+      <p>We offer a wide range of services to meet your needs.</p>
+    </div>
+  );
+}
+export default Services;
