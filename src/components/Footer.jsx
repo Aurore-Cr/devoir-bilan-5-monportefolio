a733293d4e8ad0073df9ddaf24f516adbc4ea80d
AuroreDevWeb <aurore.croizet@hotmail.fr>
2026-01-12 19:06:03 +0100

    ajout des components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer className="App-footer">
+      <p>© 2024 My Portfolio. All rights reserved.</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>Welcome to My Portfolio</h1>
+    </header>
+  );
+}
+export default Header;
