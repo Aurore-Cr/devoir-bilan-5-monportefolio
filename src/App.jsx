a54b423f997587f877cf9ed4cf5c24c0ad4deaa0
AuroreDevWeb <aurore.croizet@hotmail.fr>
2026-01-12 20:02:58 +0100

    ajout des links et correction BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,30 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Contact from "./components/Contact";
-import Mentions from "./components/Mentions";
-import Portefolio from "./components/Portefolio";
-import Home from "./components/Home";
-import Services from "./components/Services";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Portefolio from "./pages/Portefolio";
+import Contact from "./pages/Contact";
+import Mentions from "./pages/Mentions";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      {/* Other components and content can go here */}
-      <main>
-        <h1>My Portfolio</h1>
-        <nav>
-          <ul>
-            <li>
-              <BrowserRouter>
-                <Routes>
-                  <Route path="/" component={Home} />
-                  <Route exact path="/services" component={Services} />
-                  <Route exact path="/portefolio" component={Portefolio} />
-                  <Route exact path="/contact" component={Contact} />
-                  <Route exact path="/mentions" component={Mentions} />
-                </Routes>
-              </BrowserRouter>
-            </li>
-          </ul>
-        </nav>
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        {/* Other components and content can go here */}
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/portefolio" element={<Portefolio />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/mentions" element={<Mentions />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <header className="App-header">
-      <h1>Welcome to My Portfolio</h1>
+      <h1>JOHN DOE</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+          <li>
+            <Link to="/services">SERVICES</Link>
+          </li>
+          <li>
+            <Link to="/portefolio">PORTFOLIO</Link>
+          </li>
+          <li>
+            <Link to="/contact">CONTACT</Link>
+          </li>
+          <li>
+            <Link to="/mentions">MENTIONS</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
